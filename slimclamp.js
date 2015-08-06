0899578c8a0da7d54905141cc2b45a3fa7d37fa5
benelliott <bengelliott@gmail.com>
2015-08-06 21:05:21 +0100

    Improved function documentation

--- slimclamp.js
+++ slimclamp.js
@@ -1,9 +1,12 @@
-(function(){
+(function() {
+    'use strict';
+
     /**
-     * Return the current style for an element.
-     * @param {HTMLElement} elem The element to compute.
-     * @param {string} prop The style property.
-     * @returns {number}
+     * Polyfill for window.getComputedStyle which allows for the querying of
+     * an element's computed style properties.
+     * @param  elem - the element whose style is to be queried
+     * @param  prop - the name of the style property to query
+     * @return the value of elem's computed prop
      */
     function computeStyle(elem, prop) {
         if (!window.getComputedStyle) {
@@ -26,18 +29,27 @@
         return window.getComputedStyle(elem, null).getPropertyValue(prop);
     }
 
+    // Polyfill for String.trim
     if (!String.prototype.trim) {
         String.prototype.trim = function () {
             return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
         };
     }
 
+    /**
+     * Returns whether the provided string is valid.
+     * @param  string
+     * @return true if string is a valid string, else false
+     */
     var invalidString = function(string){
         return typeof string !== 'string' || string === null || string.trim() === '';
     };
 
     /**
-     * Returns the line-height of an element as an integer.
+     * Returns the computed line height of the element if it exists, else infers
+     * it from the element's computed font size.
+     * @param  element - the element whose line height to retrieve
+     * @return the element's line height
      */
     var getLineHeight = function(element) {
         var lh = computeStyle(element, 'line-height');
@@ -49,10 +61,25 @@
             return parseInt(lh);
     };
 
+    /**
+     * Returns a new string with the final numChars characters removed from
+     * the provided string.
+     * @param  string - the string to truncate
+     * @param  numChars - the number of characters to remove
+     * @return string with numChars chars removed from the end
+     */
     var removeLastChars = function(string, numChars){
-        return string.slice(0, string.length - numChars);
+        var end = string.length - numChars;
+        return end <= 0 ? '' : string.slice(0, end);
     };
 
+    /**
+     * Truncates the text contents of the provided element so that the element's
+     * height is less than or equal to maxHeightPx.
+     * @param  element - the element whose contents to truncate
+     * @param  maxHeightPx - the maximum height (in pixels) of the element
+     * @param  truncationChar - the character with which to truncate the text
+     */
     var truncate = function(element, maxHeightPx, truncationChar){
         if (!maxHeightPx || element.clientHeight <= maxHeightPx)
             return;
@@ -60,6 +87,8 @@
         var text = element.innerHTML;
 
         while (element.clientHeight > maxHeightPx) {
+            // remove one character from the text until it fits in the limits
+            // TODO: use binary search to make this more efficient
             text = removeLastChars(text, 1);
             element.innerHTML = text;
         }
@@ -73,6 +102,7 @@
 
     window.$clamp = function(element, numLines){
         if (typeof(element.style.webkitLineClamp) !== 'undefined') {
+            // browser supports clamping natively
             element.style.overflow = 'hidden';
             element.style.textOverflow = 'ellipsis';
             element.style.webkitBoxOrient = 'vertical';
@@ -80,6 +110,7 @@
             element.style.webkitLineClamp = numLines;
         }
         else {
+            // fall back to iterative clamping method
             var lineHeight = getLineHeight(element);
             if (isNaN(lineHeight))
                 lineHeight = DEFAULT_LINE_HEIGHT;
